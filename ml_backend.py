1baaa3652b56911f6461ad684d0714ce228c9077
skj_og <95894160+Jha-Shubham19@users.noreply.github.com>
2023-10-15 19:13:11 +0530

    first commit

---
+++ ml_backend.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+import pickle
+
+# 1990	1485.0	121.00	16.37	Albania	Maize	36613
+# 1	1990	1485.0	121.00	16.37	Albania	Potatoes	66667
+# 2	1990	1485.0	121.00	16.37	Albania	Rice, paddy	23333
+# 3	1990	1485.0	121.00	16.37	Albania	Sorghum	12500
+
+#loading models
+dtr = pickle.load(open('dtr.pkl','rb'))
+preprocessor = pickle.load(open('preprocessor.pkl','rb'))
+
+#creating flask app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method=='POST':
+        Year = request.form['Year']
+        average_rain_fall_mm_per_year=request.form['average_rain_fall_mm_per_year']
+        pesticides_tonnes=request.form['pesticides_tonnes']
+        avg_temp=request.form['avg_temp']
+        Area=request.form['Area']
+        Item=request.form['Item']
+
+        features = np.array([[Year,average_rain_fall_mm_per_year,pesticides_tonnes,avg_temp,Area,Item]])
+
+        transformed_features = preprocessor.transform(features)
+        predicted_value = dtr.predict(transformed_features).reshape(1,-1)
+        
+        
+        
+        return render_template('index.html',predicted_value=predicted_value)
+
+
+
+
+
+
+#python main
+if __name__=="__main__":
+    app.run(debug=True)
